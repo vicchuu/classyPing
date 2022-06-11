785052e4ec3ba0112aa0a7d462bc0be696c22912
vicchuu <vishnumaniplt@gmail.com>
2022-06-11 07:05:15 +0530

    initial Commit second commit

--- sortingAlgorithms/mainClass.py
+++ sortingAlgorithms/mainClass.py
@@ -2,9 +2,28 @@
 from sortingAlgorithms.bubbleSorting  import bubbleSorting
 
 
+import copy
+from collections import Counter
 
 
-a=[1,4,3,6,9,8,21,33,44,55,11]
+def user_info(age , **infor):
+    print("age :",age)
+    for a,v in infor.items():
+        print(a,v)
 
-s = bubbleSorting()
-print(s.sort(a))
+
+
+user_info(23,name="physics",name1="maths",name2="Science",name3="Social")
+
+
+# a={}
+#
+# #b= dict(a:10)
+#
+# a["zz"]=99
+# #a.append(90)
+# print((a))
+# #print(b)
+#
+# s = bubbleSorting()
+# #print(s.sort(a))
